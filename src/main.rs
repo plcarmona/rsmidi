b29add0d77073b84212732fc70c6c65b3a8c1e34
pedro <plcarmona@uc.cl>
2025-03-31 13:49:01 -0300

    new commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("Hello, world, from branch A! forken to a branch B");
+    println!("Hello, world, from branch A! forken to a branch B, commit to branch B, and merge branch B to branch A");
+    
 }
